84a45888e0f760af427558c02b6469924391f1ab
Ramses di Perna <15826@ma-web.nl>
2015-02-17 22:09:01 +0100

    Worked away mergeConflict

--- Assets/_Scripts/Items/DropItems/DropItem.cs
+++ Assets/_Scripts/Items/DropItems/DropItem.cs
@@ -6,26 +6,10 @@ public class DropChance : MonoBehaviour {
 	public int dropChance = 50;
 	public GameObject dropableItem;
 
-<<<<<<< HEAD:Assets/_Scripts/Items/DropItems/DropChance.cs
 	public void DropItem () {
 		int Q = Random.Range (0, 100);
 		if (Q <= dropChance) {
 			Instantiate(dropableItem,transform.position,new Quaternion(0,0,0,0));
-=======
-	void Start () {
-		int Q = Random.Range (0, 100);
-		if (Q <= dropChance) {
-			dropItemOnDeath = true;
-		}
-		else {
-			dropItemOnDeath = false;
-		}
-		DropItem ();
-	}
-	public void DropItem () {
-		if (dropItemOnDeath == true) {
-			Instantiate(dropableItem, transform.position, transform.rotation);
->>>>>>> b58a63ffb47ebd460eb4e2252be9bea202a1367d:Assets/_Scripts/Items/DropItems/DropItem.cs
 		}
 	}
 }
