94a9e7937ebc1b317f08e64d5f11ad399fe68c58
Nick van Dokkum <16072@ma-web.nl>
2015-02-17 13:48:26 +0100

    replaced button, drop chance added

---
+++ Assets/_Scripts/Items/DropItems/DropChance.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DropChance : MonoBehaviour {
+
+	public int dropChance = 50;
+	private bool dropItemOnDeath;
+	public GameObject dropableItem;
+
+	void Start () {
+		int Q = Random.Range (0, 100);
+		if (Q >= dropChance) {
+			dropItemOnDeath = true;
+		}
+		else {
+			dropItemOnDeath = false;
+		}
+	}
+	public void DropItem () {
+		if (dropItemOnDeath == true) {
+			Instantiate(dropableItem);
+		}
+	}
+}
