151d6555ed925456be070c61c9f5bd3daf500e19
Nick van Dokkum <16072@ma-web.nl>
2015-02-17 16:11:23 +0100

    added Script for StartMenu

---
+++ Assets/_Scripts/GUI/StartMenu/StartMenu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour {
+
+	public int highScore = 0;
+	public Text highscoreText;
+	// Use this for initialization
+
+	void Start (){
+		highscoreText.text = "Highscore = " + highScore;
+	}
+	public void Continue () {
+		//TODO Continue function
+	}
+
+	public void NewGame () {
+		//TODO NewGame function
+	}
+	public void NewGamePlus () {
+		//TODO NewGamePlus function
+	}
+}
