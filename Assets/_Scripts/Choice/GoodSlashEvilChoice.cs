9f36cd67289b1dde88297e068bd5a5c9647aee0f
Nick van Dokkum <16072@ma-web.nl>
2015-03-05 12:13:07 +0100

    created choice, choice door and good/evil indicator

---
+++ Assets/_Scripts/Choice/GoodSlashEvilChoice.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodSlashEvilChoice : MonoBehaviour {
+
+	public GameObject playerProgressionObject;
+	private PlayerProgression playerProgression;
+	public GameObject otherSide;
+	public bool GoodPoints;
+
+	void Start () {
+		playerProgression = playerProgressionObject.GetComponent<PlayerProgression>();
+		playerProgressionObject = null;
+	}
+	void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player") {
+			if(GoodPoints == true){
+				playerProgression.goodPoints ++;
+				print("good points +1");
+			}
+			else {
+				playerProgression.evilPoints ++;
+				print("evil points +1");
+			}
+			Destroy(otherSide.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/_Scripts/GUI/Game/GoodSlashEvilIndicator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+
+public class GoodSlashEvilIndicator : MonoBehaviour {
+
+	public Text goodGUI;
+	public Text evilGUI;
+	public GameObject playerProgressionObject;
+	private PlayerProgression playerProgression;
+
+	// Use this for initialization
+	void Start () {
+		playerProgression = playerProgressionObject.GetComponent<PlayerProgression>();
+		playerProgressionObject = null;
+		ChangeScore();
+	}
+	
+	// Update is called once per frame
+	void ChangeScore () {
+		goodGUI.text = playerProgression.goodPoints.ToString();
+		evilGUI.text = playerProgression.evilPoints.ToString();
+	}
+}
