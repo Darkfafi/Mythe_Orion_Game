f25e32ca8783ff69a2da22594e1f896aa860f22a
Nick van Dokkum <16072@ma-web.nl>
2015-03-09 16:09:10 +0100

    put old version in a folder, created new project

---
+++ NewVersion/Assets/Scripts/Movement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public int speed = 1;
+
+	public void StartMovingLeft(){
+
+	}
+	public void StopMoving(){
+
+	}
+	public void StartMovingRight(){
+
+	}
+}
