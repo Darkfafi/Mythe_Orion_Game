9ea90ea577b505ea43e18c37b196bdb4ced24902
Ramses di Perna <15826@ma-web.nl>
2015-03-11 11:02:28 +0100

    Raycast from camera done

---
+++ NewVersion/Assets/_Scripts/CameraRayChooser.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CameraRayChooser : MonoBehaviour {
+
+	public Camera cameraOne;
+	public Camera cameraTwo;
+	
+	private GameObject currentTarget;
+	// Update is called once per frame
+	void Update () {	
+		if (Input.GetMouseButtonDown (0)) {
+			Ray2D rayCast;
+			RaycastHit2D hit;
+			Vector3 worldMousePosition;
+			Camera currentCam;
+
+			if(Input.mousePosition.y > Screen.height / 2){
+				//cameraOne
+				currentCam = cameraOne;
+				Debug.Log("Player1 Cam");
+			}else{
+				//cameraTwo
+				currentCam = cameraTwo;
+				Debug.Log("Player2 Cam");
+			}
+
+			worldMousePosition = currentCam.ScreenToWorldPoint(Input.mousePosition);
+			worldMousePosition.z = 0;
+
+			hit = Physics2D.Raycast(currentCam.transform.position,worldMousePosition - currentCam.transform.position);
+
+			if(hit.collider != null){
+				if(hit.transform.gameObject.GetComponent<TouchAbleObject>() != null){
+					currentTarget = hit.transform.gameObject.name;
+					Debug.Log(hit.transform.gameObject.name);
+				}
+			}
+		}
+	}
+}
---
+++ NewVersion/Assets/_Scripts/Global/TouchAbleObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchAbleObject : MonoBehaviour {
+
+	protected bool getsTouched = false;
+
+	public virtual void StartTouchObject(){
+		getsTouched = true;
+	}
+
+	public virtual void StopTouchObject(){
+		getsTouched = false;
+	}
+}
