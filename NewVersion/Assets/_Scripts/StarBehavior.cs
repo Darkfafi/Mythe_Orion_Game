9ad67b552577cb4e9e84049571c9830ebdcfde28
Nick van Dokkum <16072@ma-web.nl>
2015-03-15 20:17:06 +0100

    added Star Beginning

---
+++ NewVersion/Assets/_Scripts/StarBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarBehavior : MonoBehaviour {
+
+	public float speed = 5;
+	public bool goDown;
+
+	void Start () {
+		if (transform.position.y >= 0) {
+			goDown = true;
+		}
+		else {
+			goDown = false;
+		}
+	}
+
+	void Update(){
+		transform.Translate (Vector3.down * Time.deltaTime * speed);
+	}
+}
---
+++ NewVersion/Assets/_Scripts/StarHolder.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarHolder : MonoBehaviour {
+
+	public GameObject star;
+	public GameObject starPrefab;
+	public bool holdingStar = false;
+
+	void Start(){
+		if (holdingStar == true && star.activeInHierarchy == false) {
+			star.SetActive(true);
+		}
+		else if (holdingStar == false && star.activeInHierarchy == true) {
+			star.SetActive(false);
+		}
+	}
+	public void ThrowStar (){
+		if (holdingStar == true) {
+			Instantiate(starPrefab, star.transform.position, star.transform.rotation);
+			holdingStar = false;
+			star.SetActive(false);
+		}
+	}
+	public void OnTriggerEnter2D (Collider2D hit){
+		if (hit.tag == "Star"){
+			if(hit.GetComponent<StarBehavior>().goDown == true && tag == "Player2"){
+				holdingStar = true;
+				star.SetActive(true);
+				Destroy(hit.gameObject);
+			}
+			else if (hit.GetComponent<StarBehavior>().goDown == false && tag == "Player1"){
+				holdingStar = true;
+				star.SetActive(true);
+				Destroy(hit.gameObject);
+			}
+		}
+		Debug.Log (hit.tag + " " + hit.GetComponent<StarBehavior>().goDown);
+	}
+	void OnMouseDown () {
+		ThrowStar ();
+	}
+}
