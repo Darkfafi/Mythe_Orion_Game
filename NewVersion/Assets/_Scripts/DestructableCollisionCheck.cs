6796d7735c0a214dc18f83df98874080728a412f
Nick van Dokkum <16072@ma-web.nl>
2015-03-16 09:37:53 +0100

    added destructable script

---
+++ NewVersion/Assets/_Scripts/DestructableCollisionCheck.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableCollisionCheck : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D hit){
+		if (hit.tag == "Star") {
+			DestroyObject();
+		}
+		if (hit.tag == "Player") {
+			hit.GetComponent<PlayerMovement>().Stop();
+		}
+	}
+	void DestroyObject(){
+		Destroy (this.gameObject);
+	}
+}
