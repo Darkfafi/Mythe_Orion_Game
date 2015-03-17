d6cfafe52f902946fa78d55657495756ed17a3da
Ramses di Perna <15826@ma-web.nl>
2015-03-17 11:03:58 +0100

    working on finish

---
+++ NewVersion/Assets/_Scripts/EndLevel.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndLevel : MonoBehaviour {
+
+	public int playersAtFinish = 0;
+	public int totalPlayers = 1;
+
+	void Start(){
+		totalPlayers = GameObject.FindGameObjectsWithTag ("Player").Length;
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		if (other.gameObject.tag == "Player") {
+			playersAtFinish += 1;
+			if(playersAtFinish == totalPlayers){
+				NextLevel();
+			}
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D other){
+		if(other.gameObject.tag == "Player"){
+			playersAtFinish -= 1;
+		}
+	}
+
+	void NextLevel(){
+		Debug.Log("Next level");
+	}
+}
