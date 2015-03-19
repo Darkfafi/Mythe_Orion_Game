d886f3db70979b24fb46c24f6845a532b618b621
Nick van Dokkum <16072@ma-web.nl>
2015-03-19 10:58:45 +0100

    fixed StarHolder

--- NewVersion/Assets/_Scripts/Star/StarHolder.cs
+++ NewVersion/Assets/_Scripts/Star/StarHolder.cs
@@ -3,26 +3,16 @@ using System.Collections;
 
 public class StarHolder : MonoBehaviour {
 
-	public GameObject star;
 	public GameObject starPrefab;
 	public bool holdingStar = false;
 	private bool starHit = false;
 
-	void Start(){
-		if (holdingStar == true && star.activeInHierarchy == false) {
-			CatchStar();
 
-		}
-		else if (holdingStar == false && star.activeInHierarchy == true) {
-			star.SetActive(false);
-		}
-	}
 	public void ThrowStar (){
 		if (holdingStar == true) {
 			GetComponentInParent<Rigidbody2D>().mass = 1;
 			Instantiate(starPrefab, transform.position, transform.rotation);
 			holdingStar = false;
-			star.SetActive(false);
 			starHit = true;
 		}
 	}
@@ -40,6 +30,5 @@ public class StarHolder : MonoBehaviour {
 	void CatchStar(){
 		GetComponentInParent<Rigidbody2D> ().mass = 10000;
 		holdingStar = true;
-		star.SetActive(true);
 	}
 }
