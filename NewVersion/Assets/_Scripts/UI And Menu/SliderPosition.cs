0cf1f4a3a8bb56a1116d9a5a0a4d90fb8134f96a
Nick van Dokkum <16072@ma-web.nl>
2015-03-19 09:06:09 +0100

    fixed timer, fixed the sliders

---
+++ NewVersion/Assets/_Scripts/UI And Menu/SliderPosition.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SliderPosition : MonoBehaviour {
+
+	public Transform player;
+	private float xPos;
+	private float lvlWidth = 0;
+	private Slider thisSlider;
+	public GameObject ground;
+	private float beginPosX;
+
+	void Start () {
+		if(player.position.x > 0){
+			lvlWidth -= player.position.x;
+		}
+		else {
+			lvlWidth += player.position.x;
+		}
+		foreach (Transform child in ground.transform)
+		{
+			lvlWidth += child.GetComponent<Renderer>().bounds.size.x;
+		}
+		thisSlider = GetComponent<Slider> ();
+	}
+	void Update () {
+		xPos = player.position.x / lvlWidth;
+		thisSlider.value = xPos;
+	}
+}
