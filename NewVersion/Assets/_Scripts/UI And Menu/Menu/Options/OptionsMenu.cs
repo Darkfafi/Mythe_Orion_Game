c5b5252d77406a42c2354c6843dce49f42752a64
Nick van Dokkum <16072@ma-web.nl>
2015-03-21 19:07:53 +0100

    Created Options Menu

---
+++ NewVersion/Assets/_Scripts/UI And Menu/Menu/Options/OptionsMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class OptionsMenu : MonoBehaviour {
+
+	private PlayerProgression playerProgression;
+
+	void Start () {
+		playerProgression = GetComponent<PlayerProgression> ();
+		if (playerProgression.nameUser == null) {
+			GetComponentInChildren<Reset>().Disable();
+		}
+	}
+	public void Sound (Slider slider) {
+		float SoundValue = slider.value;
+		PlayerPrefs.SetFloat ("SoundLevel", SoundValue);
+	}
+	public void Music (Slider slider) {
+		float MusicValue = slider.value;
+		PlayerPrefs.SetFloat ("MusicValue", MusicValue);
+	}
+	public void DestroyOptions () {
+		Destroy (gameObject);
+	}
+}
