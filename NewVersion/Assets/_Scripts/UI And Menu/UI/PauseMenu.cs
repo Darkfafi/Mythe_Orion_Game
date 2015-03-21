3d62673d8ceb8236d5501f3a1aa16e8c72ac222a
Nick van Dokkum <16072@ma-web.nl>
2015-03-21 22:27:54 +0100

    Created Pause menu

---
+++ NewVersion/Assets/_Scripts/UI And Menu/UI/PauseMenu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+
+	public GameObject OptionsPrefab;
+
+	public void Pauze () {
+		Time.timeScale = 0;
+		Debug.Log ("timeScale = " + Time.timeScale);
+	}
+	public void Resume () {
+		Time.timeScale = 1;
+		Debug.Log ("timeScale = " + Time.timeScale);
+	}
+	public void Options () {
+		(Instantiate (OptionsPrefab, transform.parent.position, transform.rotation) as GameObject).transform.parent = transform;
+	}
+	public void ReturnToLvlSelect () {
+		Application.LoadLevel ("LevelSelectionScreen");
+	}
+	public void ReturnToMenu () {
+		Application.LoadLevel ("Menu");
+	}
+}
