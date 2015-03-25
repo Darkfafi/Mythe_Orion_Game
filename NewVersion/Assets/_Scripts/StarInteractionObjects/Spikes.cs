a5dbe41774c93ffaa60dea680b8f4147902094fc
Nick van Dokkum <16072@ma-web.nl>
2015-03-25 16:57:17 +0100

    created the spike obstakel + pressure plate

---
+++ NewVersion/Assets/_Scripts/StarInteractionObjects/Spikes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spikes : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D hit) {
+		if (hit.tag == "Player") {
+			//TODO spikes hit
+			print("spikes hit");
+		}
+	}
+}
