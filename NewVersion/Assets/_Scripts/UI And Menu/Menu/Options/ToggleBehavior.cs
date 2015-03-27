6ce2a942ded952b31de2dc1bd7196674de9bb27f
Nick van Dokkum <16072@ma-web.nl>
2015-03-27 13:22:23 +0100

    made the options menu better

---
+++ NewVersion/Assets/_Scripts/UI And Menu/Menu/Options/ToggleBehavior.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ToggleBehavior : MonoBehaviour {
+
+	private Toggle toggle;
+	public SliderUpdater sliderUpdater;
+
+	void Start () {
+		toggle = GetComponent<Toggle> ();
+	}
+	public void ValueChanged () {
+		if(toggle.isOn == false){
+			sliderUpdater.UpdateSlider (0);
+		}
+		else if (sliderUpdater.SliderValue() == 0) {
+			sliderUpdater.UpdateSlider(100);
+		}
+		sliderUpdater.UpdateText ();
+	}
+	public void CheckValue (Slider slider) {
+		if (slider.value != 0 && toggle.isOn == false) {
+			toggle.isOn = true;
+		}
+	}
+}
