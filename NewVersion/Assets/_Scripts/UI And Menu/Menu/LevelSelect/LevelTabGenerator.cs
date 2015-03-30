0f3cf16780103be7ebec66f0902dcd43a755e227
Ramses di Perna <15826@ma-web.nl>
2015-03-30 16:40:03 +0200

    Added last 2 levels

--- NewVersion/Assets/_Scripts/UI And Menu/Menu/LevelSelect/LevelTabGenerator.cs
+++ NewVersion/Assets/_Scripts/UI And Menu/Menu/LevelSelect/LevelTabGenerator.cs
@@ -6,18 +6,13 @@ public class LevelTabGenerator : MonoBehaviour {
 	public GameObject levelTabPrefab;
 		
 	void Awake(){
-		AddLevelTabs (7);
+		AddLevelTabs (9);
 	}
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void AddLevelTabs (int amount) {
 		GameObject levelTab;
 		for (int i = 0; i < amount; i++) {
-			levelTab = Instantiate(levelTabPrefab,new Vector3((transform.position.x - 400) + i * 120f,transform.position.y + 100,0),Quaternion.identity) as GameObject;
+			levelTab = Instantiate(levelTabPrefab,new Vector3((transform.position.x - 480) + i * 120f,transform.position.y + 100,0),Quaternion.identity) as GameObject;
 
 			levelTab.transform.SetParent(gameObject.transform,false);
 
