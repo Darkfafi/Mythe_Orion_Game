fb81d40cd9dee12e347ed31ac8beece3431fe83c
Ramses di Perna <15826@ma-web.nl>
2015-03-30 16:04:12 +0200

    Fixed some bugs and remade tutorial level

---
+++ NewVersion/Assets/_Scripts/TutorialEvents/TextOnEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class TextOnEnter : MonoBehaviour {
+
+	public GameObject targetPlayer;
+	public string textAfterEvent;
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.gameObject == targetPlayer.gameObject) {
+			GetComponent<Text>().text = textAfterEvent;
+			Destroy(this);
+		}
+	}
+}
--- NewVersion/Assets/_Scripts/UI And Menu/Menu/CreateAccountScreen/CreateAccount.cs
+++ NewVersion/Assets/_Scripts/UI And Menu/Menu/CreateAccountScreen/CreateAccount.cs
@@ -15,13 +15,17 @@ public class CreateAccount : MonoBehaviour {
 
 	public void SubmitName(){
 		string usernameGiven = GameObject.Find("UserNameInput").GetComponent<Text>().text;
-		if(usernameGiven.Length < 3){
-			reportText.GetComponent<Text>().text = "Account name must be at least 3 characters long";
-		}else if(usernameGiven.Length > 15){
-			reportText.GetComponent<Text>().text = "Account name can't be longer than 15 characters";
-		}
-		else{
-			StartCreateAccount(usernameGiven);
+		if(CheckNameOnWrongChar(usernameGiven)){
+			if(usernameGiven.Length < 3){
+				reportText.GetComponent<Text>().text = "Account name must be at least 3 characters long";
+			}else if(usernameGiven.Length > 15){
+				reportText.GetComponent<Text>().text = "Account name can't be longer than 15 characters";
+			}
+			else{
+				StartCreateAccount(usernameGiven);
+			}
+		}else{
+			reportText.GetComponent<Text>().text = "You can't use '~' in your name";
 		}
 	}
 
@@ -32,5 +36,16 @@ public class CreateAccount : MonoBehaviour {
 		playerProg.levelsCompleteWithTime = new Dictionary<int, int> (); 
 		GetComponent<SaveLoadData> ().Save ();
 		Application.LoadLevel ("Menu");
+	}
+
+	bool CheckNameOnWrongChar(string name){
+		bool result = true;
+		char splitchar = '~';
+		for(int i = 0; i < name.Length; i++){
+			if(name[i] == splitchar){
+				result = false;
+			}
+		}
+		return result;
 	}
 }
